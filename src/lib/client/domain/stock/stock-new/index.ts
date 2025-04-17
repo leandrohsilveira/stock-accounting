d57c44c45e734bb5ae2f81f61946cde70442f7e6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-04-17 12:08:36 -0300

    add stock new component

---
+++ src/lib/client/domain/stock/stock-new/index.ts
@@ -0,0 +1,1 @@
+export { default as StockNew } from './customer-new.svelte'
---
+++ src/lib/client/domain/stock/stock-new/stock-new.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Title from '$lib/client/components/title.svelte'
+  import { useTranslate } from '$lib/lang'
+  import type { StockMutationData } from '$lib/shared/models'
+  import type { ErrorsMap } from '$lib/shared/util/form'
+  import { StockForm } from '../stock-form'
+  import { literals } from './stock-new.i18n'
+
+  interface Props {
+    back: string
+    errors: ErrorsMap<StockMutationData>
+    loading?: boolean
+  }
+
+  const t = useTranslate(literals)
+
+  let { back, errors, loading }: Props = $props()
+</script>
+
+<Title name={$t('title')} />
+
+<StockForm {back} {loading} title={$t('title')} submitLabel={$t('submit')} {errors} />
