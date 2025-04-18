6e65a2513f2fe94fab109cdfd368649b1f6eaacf
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-04-17 22:18:03 -0300

    layout tweaks

--- src/lib/client/components/navbar/navbar.i18n.ts
+++ src/lib/client/components/navbar/navbar.i18n.ts
@@ -3,6 +3,7 @@ import { defineLiterals } from '$lib/lang'
 export const literals = defineLiterals({
   'en-us': {
     login: 'Login',
+    profile: 'Profile',
     logout: 'Logout',
   },
 })
--- src/lib/client/components/navbar/navbar.svelte
+++ src/lib/client/components/navbar/navbar.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { useTranslate } from '$lib/lang'
   import type { UserData } from '$lib/shared/models'
+  import UserIcon from '$lib/client/icons/icon_user.svg?component'
+  import LoginIcon from '$lib/client/icons/icon_login.svg?component'
+  import EditIcon from '$lib/client/icons/icon_edit.svg?component'
+  import LogoutIcon from '$lib/client/icons/icon_logout.svg?component'
   import { literals } from './navbar.i18n'
 
   interface Props {
@@ -17,30 +21,40 @@
 <header class="navbar bg-primary text-primary-content justify-center">
   <div class="app-margin flex items-center justify-between gap-4">
     <h1 class="text-xl">Stock Accounting</h1>
-    {#if currentUser !== null}
-      <button
-        class="btn btn-ghost btn-primary"
-        popovertarget="auth-popover"
-        style="anchor-name:--auth-anchor;"
-      >
-        {currentUser.displayName}
-      </button>
-      <ul
-        class="dropdown menu rounded-box bg-base-100 w-52 shadow-sm"
-        popover
-        id="auth-popover"
-        style="position-anchor:--auth-anchor;"
-      >
-        <li>
-          <a href={updateUrl}>Profile</a>
-        </li>
-        <li>
-          <button type="submit" form={formId}>{$t('logout')}</button>
-        </li>
-      </ul>
-    {:else}
-      <a href="/login?next={currentUrl.pathname}" class="btn btn-ghost btn-primary">{$t('login')}</a
-      >
-    {/if}
+
+    <ul class="menu menu-horizontal">
+      <li>
+        {#if currentUser !== null}
+          <button popovertarget="auth-popover" style="anchor-name:--auth-anchor;">
+            <UserIcon fill="currentColor" />
+            {currentUser.displayName}
+          </button>
+          <ul
+            class="dropdown menu rounded-box bg-base-100 w-52 shadow-sm"
+            popover
+            id="auth-popover"
+            style="position-anchor:--auth-anchor;"
+          >
+            <li>
+              <a href={updateUrl}>
+                <EditIcon fill="currentColor" />
+                {$t('profile')}
+              </a>
+            </li>
+            <li>
+              <button type="submit" form={formId}>
+                <LogoutIcon fill="currentColor" />
+                {$t('logout')}
+              </button>
+            </li>
+          </ul>
+        {:else}
+          <a href="/login?next={currentUrl.pathname}">
+            <LoginIcon fill="currentColor" />
+            {$t('login')}
+          </a>
+        {/if}
+      </li>
+    </ul>
   </div>
 </header>
